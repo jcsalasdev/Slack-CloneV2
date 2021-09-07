8f43479d8cbcc40bed4d68605370bbfa6aef8670
Oliver Marquez <olivermarquez0@gmail.com>
2021-09-07 20:32:02 +0800

    maker added

--- src/components/Client/client.js
+++ src/components/Client/client.js
@@ -1,31 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteMatch, Route, Switch } from "react-router-dom";
 import { useGetUser } from "../../context/UserProvider";
 import ChatBar from "../ChatBar/ChatBar";
 import Messages from "../Messages/Messages";
 import AsideNav from "./Aside";
+import Maker from "../Maker/Maker";
 import "./client.css";
+/* import Users from "../Users/Users"; */
 
 export default function Client() {
-	const user = useGetUser()
-	if (!user) {
-		return (
-			<div className="no-user_container">
-				<div className="no-user">
-					<h1 className="no-user_title">Account has Sign Out</h1>
-					<Link to="/">Back to Sign In</Link>
-				</div>
-			</div>)
-	}
-	return (
-		<div className="client-container">
-			<AsideNav />
-			<div className="chat-container">
+  const user = useGetUser();
+  const { url } = useRouteMatch();
 
-				<Messages />
-				<ChatBar />
+  if (!user) {
+    return (
+      <div className="no-user_container">
+        <div className="no-user">
+          <h1 className="no-user_title">Account has Sign Out</h1>
+          <Link to="/">Back to Sign In</Link>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="client-container">
+      <AsideNav />
 
-			</div>
-		</div>
-
-	);
+      <Switch >
+        <Route exact path={`${url}/chats`}>
+          <div className="chat-container">
+            <Messages />
+            <ChatBar />
+          </div>
+        </Route>
+        <Route path={`${url}/new-channel`} component={Maker} />
+      </Switch>
+    </div>
+  );
 }
